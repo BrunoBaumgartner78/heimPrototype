324008444a845b63d6c8326b8ab96b4303e31e24
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-07-17 13:21:52 +0200

    verschiedenes

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 document.getElementById('menuToggle').addEventListener('click', () => {
   document.getElementById('navLinks').classList.toggle('active');
 });
+
+  const toggleBtn = document.getElementById("darkModeToggle");
+  const icon = toggleBtn.querySelector("i");
+
+  // Dark Mode Status aus Local Storage laden
+  if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  }
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    // Icon wechseln
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+      localStorage.setItem("darkMode", "disabled");
+    }
+  });
+
